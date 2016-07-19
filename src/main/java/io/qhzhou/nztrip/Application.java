5b9cadd3fd4652e28c9331288cf6d42a4d557732
qianhao.zhou <z.qianhao@gmail.com>
2016-07-19 16:11:53 +0800

    add signin.html

--- src/main/java/io/qhzhou/nztrip/Application.java
+++ src/main/java/io/qhzhou/nztrip/Application.java
@@ -1,14 +1,12 @@
 package io.qhzhou.nztrip;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by qianhao.zhou on 7/17/16.
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
 
 
