99d97f14719f17a3cd82bfe99b102075390e3aa0
qianhao.zhou <z.qianhao@gmail.com>
2016-07-19 16:22:33 +0800

    minor fix

--- src/main/java/io/qhzhou/nztrip/controller/TestController.java
+++ src/main/java/io/qhzhou/nztrip/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${server.address}")
+    @Value("${my.config}")
     private String address;
 
     @RequestMapping("test")
