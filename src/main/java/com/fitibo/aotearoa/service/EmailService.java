1b5d4b77a35a1886a3fc4e239c76b06da8606fe7
qianhao.zhou <z.qianhao@gmail.com>
2016-08-25 17:54:13 +0800

    fix

--- src/main/java/com/fitibo/aotearoa/service/EmailService.java
+++ src/main/java/com/fitibo/aotearoa/service/EmailService.java
@@ -3,6 +3,8 @@ package com.fitibo.aotearoa.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -19,9 +21,15 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.email-enabled}")
+    private Boolean enabled;
+
     private final static Logger logger = LoggerFactory.getLogger(EmailService.class);
 
     public void sendEmail(String from, String subject, String content, String to) throws MessagingException {
+        if (!enabled) {
+            return;
+        }
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "utf-8");
         messageHelper.setFrom(from);
