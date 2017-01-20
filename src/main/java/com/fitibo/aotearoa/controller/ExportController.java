6f8254d4b5f505a99f558c122fdd541371725066
qianhao.zhou <z.qianhao@gmail.com>
2017-01-21 03:58:37 +0800

    fix bug

--- src/main/java/com/fitibo/aotearoa/controller/ExportController.java
+++ src/main/java/com/fitibo/aotearoa/controller/ExportController.java
@@ -317,14 +317,4 @@ public class ExportController extends AuthenticationRequiredController {
     }
   }
 
-  public static void main(String[] args) {
-    final Map<Integer, Integer> result = Maps.newHashMap();
-    Observable.range(0, 10).groupBy(input -> input % 2).subscribe(groupedObservable -> {
-      groupedObservable.reduce((v1, v2) -> {
-        System.out.println("v1:" + v1 + " v2:" + v2);
-        return v1 + v2;
-      }).collect(() -> result, (map, value) -> map.put(groupedObservable.getKey(), value)).subscribe();
-    });
-    System.out.println(result);
-  }
 }
