7d81f04942085db2345e837c0db5c2c45a97c47c
qianhao.zhou <z.qianhao@gmail.com>
2017-02-02 12:07:20 +0800

    format

--- src/main/java/com/fitibo/aotearoa/service/impl/EmailServiceImpl.java
+++ src/main/java/com/fitibo/aotearoa/service/impl/EmailServiceImpl.java
@@ -4,7 +4,9 @@ import com.fitibo.aotearoa.mapper.EmailQueueMapper;
 import com.fitibo.aotearoa.model.Email;
 import com.fitibo.aotearoa.service.EmailService;
 import com.fitibo.aotearoa.service.UtilityService;
-
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
+import javax.mail.internet.MimeMessage;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,109 +16,105 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
-import javax.annotation.PostConstruct;
-import javax.mail.internet.MimeMessage;
-
 /**
  * Created by qianhao.zhou on 9/1/16.
  */
 @Service("mailService")
 public class EmailServiceImpl implements EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+  @Autowired
+  private JavaMailSender mailSender;
 
-    @Value("${spring.email-enabled}")
-    private Boolean enabled;
+  @Value("${spring.email-enabled}")
+  private Boolean enabled;
 
-    @Autowired
-    private EmailQueueMapper emailQueueMapper;
+  @Autowired
+  private EmailQueueMapper emailQueueMapper;
 
-    @Autowired
-    private UtilityService utilityService;
+  @Autowired
+  private UtilityService utilityService;
 
-    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+  private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
-    @PostConstruct
-    public void init() {
-        utilityService.getScheduledExecutorService().scheduleAtFixedRate(() -> {
-            logger.info("start auto-resend email task");
-            try {
-                for (Email email : emailQueueMapper.findAllFailedEmails()) {
-                    logger.info("resend email id:" + email.getId());
-                    new SendEmailTask(email).run();
-                }
-            } catch (Exception e) {
-                logger.error("fail to run auto-resend task", e);
-            }
-            logger.info("finish auto-resend email task");
-        }, 0, 5, TimeUnit.MINUTES);
+  @PostConstruct
+  public void init() {
+    utilityService.getScheduledExecutorService().scheduleAtFixedRate(() -> {
+      logger.info("start auto-resend email task");
+      try {
+        for (Email email : emailQueueMapper.findAllFailedEmails()) {
+          logger.info("resend email id:" + email.getId());
+          new SendEmailTask(email).run();
+        }
+      } catch (Exception e) {
+        logger.error("fail to run auto-resend task", e);
+      }
+      logger.info("finish auto-resend email task");
+    }, 0, 5, TimeUnit.MINUTES);
+  }
+
+  @Override
+  public void send(int orderId, String from, String to, String subject, String content) {
+    final Email email = new Email();
+    email.setContent(content);
+    email.setFrom(from);
+    email.setTo(to);
+    email.setSubject(subject);
+    email.setOrderId(orderId);
+    emailQueueMapper.create(email);
+    utilityService.getScheduledExecutorService()
+        .schedule(new SendEmailTask(email), 1, TimeUnit.SECONDS);
+  }
+
+  private class SendEmailTask implements Runnable {
+
+    private final Email email;
+
+    private SendEmailTask(Email email) {
+      this.email = email;
     }
 
     @Override
-    public void send(int orderId, String from, String to, String subject, String content) {
-        final Email email = new Email();
-        email.setContent(content);
-        email.setFrom(from);
-        email.setTo(to);
-        email.setSubject(subject);
-        email.setOrderId(orderId);
-        emailQueueMapper.create(email);
-        utilityService.getScheduledExecutorService().schedule(new SendEmailTask(email), 1, TimeUnit.SECONDS);
-    }
-
-    private class SendEmailTask implements Runnable {
-
-        private final Email email;
-
-        private SendEmailTask(Email email) {
-            this.email = email;
-        }
-
-        @Override
-        public void run() {
-            if (sendMail(email)) {
-                try {
-                    emailQueueMapper.updateSucceed(email.getId());
-                } catch (Exception e) {
-                    logger.error("fail to update email status to succeed, mail id:" + email.getId(), e);
-                }
-            } else {
-                try {
-                    emailQueueMapper.updateRetry(email.getId());
-                } catch (Exception e) {
-                    logger.error("fail to update email retry, mail id:" + email.getId(), e);
-                }
-            }
-        }
-    }
-
-    private boolean sendMail(Email email) {
+    public void run() {
+      if (sendMail(email)) {
         try {
-            String to = email.getTo();
-            if (StringUtils.isEmpty(to)) {
-                return true;
-            }
-            if (!enabled) {
-                to = "will@ctofunds.com";
-            }
-            String[] receivers = to.split(";");
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper messageHelper = new MimeMessageHelper(message, false, "utf-8");
-            messageHelper.setFrom(email.getFrom());
-            messageHelper.setSubject(email.getSubject());
-            messageHelper.setText(email.getContent(), true);
-            messageHelper.setTo(receivers);
-            message.saveChanges();
-            message.setHeader("Content-Transfer-Encoding", "base64");
-            mailSender.send(message);
-            return true;
+          emailQueueMapper.updateSucceed(email.getId());
         } catch (Exception e) {
-            logger.error("failed to send mail id:" + email.getId(), e);
-            return false;
+          logger.error("fail to update email status to succeed, mail id:" + email.getId(), e);
         }
+      } else {
+        try {
+          emailQueueMapper.updateRetry(email.getId());
+        } catch (Exception e) {
+          logger.error("fail to update email retry, mail id:" + email.getId(), e);
+        }
+      }
     }
+  }
+
+  private boolean sendMail(Email email) {
+    try {
+      String to = email.getTo();
+      if (StringUtils.isEmpty(to)) {
+        return true;
+      }
+      if (!enabled) {
+        to = "will@ctofunds.com";
+      }
+      String[] receivers = to.split(";");
+      MimeMessage message = mailSender.createMimeMessage();
+      MimeMessageHelper messageHelper = new MimeMessageHelper(message, false, "utf-8");
+      messageHelper.setFrom(email.getFrom());
+      messageHelper.setSubject(email.getSubject());
+      messageHelper.setText(email.getContent(), true);
+      messageHelper.setTo(receivers);
+      message.saveChanges();
+      message.setHeader("Content-Transfer-Encoding", "base64");
+      mailSender.send(message);
+      return true;
+    } catch (Exception e) {
+      logger.error("failed to send mail id:" + email.getId(), e);
+      return false;
+    }
+  }
 
 }
