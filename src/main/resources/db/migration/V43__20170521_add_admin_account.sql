28a974a03203fb42deefdf224b838c8cc912be81
qianhao.zhou <z.qianhao@gmail.com>
2017-05-21 15:46:04 +0800

    add admin account

---
+++ src/main/resources/db/migration/V43__20170521_add_admin_account.sql
@@ -0,0 +1,5 @@
+INSERT INTO `admin` (`user`, `pass`)
+VALUES
+	('meizhu', '488ceb60e38a64ddb8914cbde562a2cc'),
+	('danke', '820bdbefcd00b6ddf500279e6e8d3194'),
+	('jessica', 'f86b2e5df26b5f8e1fa0cf8eb9ca7bc2');
