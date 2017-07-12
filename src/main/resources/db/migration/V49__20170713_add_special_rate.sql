9da20410fefb6c0784c7bad433a4b121ddaa5416
qianhao.zhou <z.qianhao@gmail.com>
2017-07-13 01:01:53 +0800

    add special rate

---
+++ src/main/resources/db/migration/V49__20170713_add_special_rate.sql
@@ -0,0 +1,3 @@
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('HM0001', 0, 28, 13);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('HM0002', 0, 28, 13);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('HM0006', 0, 28, 13);
