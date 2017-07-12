578f9a34a45110c7d82d9a38f6b9dd2a3d30448b
qianhao.zhou <z.qianhao@gmail.com>
2017-07-12 23:38:40 +0800

    add special rate

---
+++ src/main/resources/db/migration/V48__20170712_add_special_rate.sql
@@ -0,0 +1,12 @@
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('AD001', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('AD002', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('AD003', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('RJ100', 0, 28, 12);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('BP001', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('BP002', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('BP003', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('BP004', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('FFHL01', 0, 28, 12);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('PW001', 0, 28, 15);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('SB001', 0, 28, 0);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('OH001', 0, 28, 5);
