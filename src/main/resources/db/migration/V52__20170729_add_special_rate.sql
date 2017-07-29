91684cb2f78af58d6117769cbb00f7ec39b1f35b
qianhao.zhou <z.qianhao@gmail.com>
2017-07-29 14:48:30 +0800

    add special rate for zuzuche

---
+++ src/main/resources/db/migration/V52__20170729_add_special_rate.sql
@@ -0,0 +1,3 @@
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('AS001', 0, 28, 15);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('UF001', 0, 28, 10);
+insert into special_rate(`sku`, `sku_id`, `agent_id`, `discount`) values('OH001', 0, 28, 10);
